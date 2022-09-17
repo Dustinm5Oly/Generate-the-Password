72a94f0854bb0969747d4975c4d5c2d7b53bf25d
Dustinm5Oly <111804479+Dustinm5Oly@users.noreply.github.com>
2022-09-16 18:43:37 -0700

    created the four other prompts to select which characters the user wants associated with the password and created console log to log

--- Assets/script.js
+++ Assets/script.js
@@ -10,7 +10,31 @@ function generatePassword() {
     } else {
       alert("Please select a number between 8 and 128 characters"); // this else statement only occurs if the user selects a number outside of 8 adn 128
     };
+    
+    userPrompt1 = confirm ("Would you like your password to include LowerCase letters?"); // this confirm allows the user to select if they want lowercase letters in their password
+    if (userPrompt1){
+      userPrompt1 = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]; // userPrompt1 gives the array of characters that can be selected
+      randomArray= randomArray.concat(userPrompt1); // concat is merging the userPrompt1 with the randomArray
+    };
 
+    userPrompt2 = confirm ("Would you like your password to include UpperCase letters?");
+    if (userPrompt2){
+      userPrompt2 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
+      randomArray= randomArray.concat(userPrompt2);
+    };
+
+    userPrompt3 = confirm ("Would you like your password to include Special Characters?");
+    if (userPrompt3){
+      userPrompt3 = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
+      randomArray= randomArray.concat(userPrompt3);
+    };
+
+    userPrompt4 = confirm ("Would you like your password to include numbers?")
+    if (userPrompt4){
+      userPrompt4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+      randomArray= randomArray.concat(userPrompt4);
+    }
+    console.log(randomArray); // this is logging the randomArray that contains all the options of characters that the user selected
 }
 
 
